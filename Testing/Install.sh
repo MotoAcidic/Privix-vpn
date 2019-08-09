7e6c3c01f88e860f1e0bea9bbb4b80072cf11b8e
TFinch <travisfinch01@gmail.com>
2019-08-08 21:19:59 -0400

    Clean Up

--- Testing/Install.sh
+++ Testing/Install.sh
@@ -4,23 +4,19 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 # Ask user if they would like to install the vpn software after node install.
-    echo -e "${GREEN}Would you like to install Privix VPN? Y or N${GREEN}> "
+	echo "${GREEN}Please read the terms of service document that can located here: Privix.io${GREEN}"
+    echo -e "Would you like to install Privix VPN and accept the Terms? Y or N}> "
 read USER_INPUT
 if [ $USER_INPUT == "Y" ] ||
    [ $USER_INPUT == "y" ]; then
-	echo "Please read the terms of service document that can located here: Privix.io"
-	echo "If you accept and acknowledge the terms Press Y if not Press N. > "
-	read AGREEMENT
-		elif [ $AGREEMENT == "Y" ] ||
-			 [ $AGREEMENT == "y" ]; then
-			cd
-			cd Privix-vpn/VPN/privixvpn/
-			bash install.sh
-		else  
-			echo Vpx configuration file created successfully. 
-			echo Vpx Server Started Successfully using the command ./privixd -daemon
-			echo If you get a message asking to rebuild the database, please hit Ctr + C and run ./privixd -daemon -reindex
-			echo If you still have further issues please reach out to support in our Discord channel. 
-			echo Please use the following Private Key when setting up your wallet: $GENKEY
-			exit 0
+		cd
+		cd Privix-vpn/VPN/privixvpn/
+		bash install.sh
+	else  
+		echo Vpx configuration file created successfully. 
+		echo Vpx Server Started Successfully using the command ./privixd -daemon
+		echo If you get a message asking to rebuild the database, please hit Ctr + C and run ./privixd -daemon -reindex
+		echo If you still have further issues please reach out to support in our Discord channel. 
+		echo Please use the following Private Key when setting up your wallet: $GENKEY
+	exit 0
 fi
