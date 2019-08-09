ad4e0c40301e6563e77abbca7f4ac823baad2729
TFinch <travisfinch01@gmail.com>
2019-08-08 22:12:05 -0400

    text

--- VPN/VPN_Selection_Install.sh
+++ VPN/VPN_Selection_Install.sh
@@ -6,7 +6,7 @@ WIDTH=40
 CHOICE_HEIGHT=6
 BACKTITLE="VPX VPN Setup Wizard"
 TITLE="VPX VPN Setup"
-MENU="Choose one of the following vpn options to install. Keep in mind you MUST be running a Privix Node on this server for this to work!"
+MENU="Choose from below: You MUST be running a Privix Node on this server for this to work!"
 
 OPTIONS=(1 "ipsec"
 		 2 "pptp"
